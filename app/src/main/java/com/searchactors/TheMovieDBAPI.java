15305e8a52ba1ffc985830e350e65c84b27936a4
gokberkerses <erses.gokberk@gmail.com>
2019-03-16 10:39:30 +0300

    dynamic URL in requests

--- app/src/main/java/com/searchactors/TheMovieDBAPI.java
+++ app/src/main/java/com/searchactors/TheMovieDBAPI.java
@@ -2,19 +2,22 @@ package com.searchactors;
 
 import java.lang.annotation.Target;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface TheMovieDBAPI {
     String BASE_URL = "https://api.themoviedb.org/3/" ;
 
-    Integer currentPopularPage = 1 ;
-    Integer currentSearchPage = 1 ;
+    
+    @GET("person/popular?api_key=90f9f56e299e21f06338b0197a5ff6f6&language=en-US&page={currentPage}")
+    Call<ResultsPage> getPopularResults(
+            @Path("currentPage") String currentPage
+    ) ;
 
-    //use @PATH ? dig annotations. 
-    @GET("person/popular?api_key=90f9f56e299e21f06338b0197a5ff6f6&language=en-US&page=" + currentPopularPage)
-    ResultsPage getPopularResults() ;
-
-    @GET("search/person?api_key=<<api_key>>&language=en-US&page=" + currentSearchPage + "&include_adult=false")
-    ResultsPage getSearchResults() ;
+    @GET("search/person?api_key=90f9f56e299e21f06338b0197a5ff6f6&language=en-US&page={currentPage}&include_adult=false")
+    ResultsPage getSearchResults(
+            @Path("currentPage") String currentPage
+    ) ;
 
 }
